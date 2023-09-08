a883cc3b37d733c7f108a3a28d7beda8f91c2517
vyshu1234567 <vyshnavibalu10b@gmail.com>
2023-09-08 16:03:57 +0530

    adding demo.js file

---
+++ dom.js
@@ -0,0 +1,6 @@
+// QUERYSELECTOR //
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello world'
