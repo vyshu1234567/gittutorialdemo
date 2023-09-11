399d0dca16dc0ba7953660ed39bea700d6a1f8aa
vyshu1234567 <vyshnavibalu10b@gmail.com>
2023-09-11 12:14:37 +0530

    adding dom.js file

--- dom.js
+++ dom.js
@@ -1,6 +1,32 @@
 // QUERYSELECTOR //
-var header = document.querySelector('#main-header');
-header.style.borderBottom = 'solid 4px #ccc';
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
 
-var input = document.querySelector('input');
-input.value = 'Hello world';
+// var input = document.querySelector('input');
+// input.value = 'Hello world'
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND"
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'coral';
+
+// QUERYSELECTORALL //
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i = 0; i < odd.length;i++){
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
+}
